54944ade5316959fd7cccdb62c8d3f565bd44c27
Mr01Gribobas <maks-korzun@bk.ru>
2026-02-01 15:43:14 +0300

    Добавьте файлы проекта.

---
+++ working_with_timescale_data/Models/TimescaleDb.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.PortableExecutable;
+
+namespace working_with_timescale_data.Models;
+
+public class TimescaleDb: DbContext
+{
+    public TimescaleDb(DbContextOptions<TimescaleDb> options):base (options)
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        String connect = "Host=localhost;Port=5432;Database=TimescaleDb;Username=postgres;Password=42924870";
+        optionsBuilder.UseNpgsql();
+    }
+}
